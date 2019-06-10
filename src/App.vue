379e83b5b954418603bd4c828f0e17453bdc5654
morlz <morlz@mail.ru>
2019-06-10 19:48:53 +0300

    Teenagerst prompt

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,22 @@
 
 <script>
 export default {
-	name: 'App'
+	name: 'App',
+	mounted () {
+		setTimeout(() => {
+			this.$q.dialog({
+				title: 'Подтвердите, что вам больше 18 лет',
+				message: '',
+				cancel: 'Уйти',
+				ok: 'Мне есть 18 лет'
+			}).onCancel(() => {
+				window
+					.open("about:blank", "_self")
+					.close()
+			})
+		}, 500)
+
+	}
 }
 </script>
 
